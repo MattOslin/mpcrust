af407f48bb7a853fc3b4716dd06a6080af518b24
MattOslin <matt@oslin.net>
2022-04-14 14:11:25 -0700

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,102 @@
 use optimization_engine::{constraints, panoc, FunctionCallResult, Optimizer, Problem};
+use std::{fmt::Debug, ops::AddAssign};
 
-#[derive(Debug)]
-pub struct Cost<GradientType, CostType>
-where
-    GradientType: Fn(&[f64], &mut [f64]),
-    CostType: Fn(&[f64], &mut f64),
-{
-    stage_cost: CostType,
-    stage_cost_gradient: GradientType,
-    terminal_cost: CostType,
-    terminal_cost_gradient: GradientType,
+pub trait Cost {
+    fn stage_cost(&self, x: &[f64], u: &[f64]) -> f64;
+    // TODO: Combine?
+    // grad = grad + dcost/dx
+    fn stage_grad_x(&self, x: &[f64], u: &[f64], grad: &mut [f64]);
+    // grad = grad + dcost/du
+    fn stage_grad_u(&self, x: &[f64], u: &[f64], grad: &mut [f64]);
+    fn terminal_cost(&self, x: &[f64]) -> f64;
+    fn terminal_grad(&self, x: &[f64], grad: &mut [f64]);
+}
+
+pub trait Dynamics {
+    fn step(&self, x: &[f64], u: &[f64], x_next: &mut [f64]);
+    // Apply the operation output = dStep/dx * input.
+    fn apply_x_grad(&self, x: &[f64], u: &[f64], input: &[f64], output: &mut [f64]);
+    // Apply the operation output = dStep/du * input.
+    fn apply_u_grad(&self, x: &[f64], u: &[f64], input: &[f64], output: &mut [f64]);
 }
 
 #[derive(Debug)]
-pub struct MPC<CostType, DynamicsType>
+pub struct MPC<DynamicsType, CostType>
 where
-    DynamicsType: Fn(&[f64], &mut [f64]),
+    DynamicsType: Dynamics,
+    CostType: Cost,
 {
-    num_states: usize,
+    initial_state: Vec<f64>,
     num_controls: usize,
     num_stages: usize,
     dynamics: DynamicsType,
     cost: CostType,
 }
 
-impl<CostType, DynamicsType> MPC<CostType, DynamicsType>
+pub fn add_to<T>(a: &mut [T], b: &[T])
 where
-    DynamicsType: Fn(&[f64], &mut [f64]),
+    T: Copy + AddAssign,
+{
+    assert!(a.len() == b.len());
+    a.iter_mut().zip(b.iter()).map(|(x, y)| (*x) += *y);
+}
+
+impl<DynamicsType, CostType> MPC<DynamicsType, CostType>
+where
+    DynamicsType: Dynamics,
+    CostType: Cost,
 {
     fn cost(&self, u: &[f64], c: &mut f64) -> FunctionCallResult {
-        // TODO: single shot.
+        *c = 0.0;
+        let mut x = self.initial_state.clone();
+        let mut x_next = Vec::with_capacity(self.initial_state.len());
+        for i in 0..self.num_stages {
+            let u_i = i * self.num_controls;
+            let u = &u[u_i..u_i + self.num_controls];
+            *c += self.cost.stage_cost(&x, u);
+            self.dynamics.step(&x, u, &mut x_next);
+            x.swap_with_slice(&mut x_next);
+        }
+        *c += self.cost.terminal_cost(&x);
         Ok(())
     }
 
     fn grad(&self, u: &[f64], grad: &mut [f64]) -> FunctionCallResult {
-        // TODO: rollout followed by backprop.
+        // TODO: Cache x allocation?
+        let n = self.initial_state.len();
+        let mut x = Vec::with_capacity(n * (self.num_stages + 1));
+        x[0..n].clone_from_slice(&self.initial_state);
+        for i in 0..self.num_stages {
+            let x_i = i * n;
+            let u_i = i * self.num_controls;
+            self.dynamics.step(
+                &x[x_i..x_i + n],
+                &u[u_i..u_i + self.num_controls],
+                &mut x[x_i + n..x_i + 2 * n],
+            );
+        }
+        let x = x;
+
+        let mut p_next = &mut Vec::with_capacity(n)[..];
+        let mut p = &mut Vec::with_capacity(n)[..];
+        self.cost
+            .terminal_grad(&x[n * self.num_stages..n * (self.num_stages + 1)], &mut p);
+
+        for i in (0..self.num_stages).rev() {
+            let x_i = i * n;
+            let u_i = i * self.num_controls;
+            let x = &x[x_i..x_i + n];
+            let u = &u[u_i..u_i + self.num_controls];
+            let du = &mut grad[u_i..u_i + self.num_controls];
+
+            p_next.swap_with_slice(p);
+            self.dynamics.apply_x_grad(x, u, &p_next, &mut p);
+            // p_next used as temp here.
+            self.cost.stage_grad_x(x, u, &mut p);
+            let p = p;
+            self.dynamics.apply_u_grad(x, u, &p, du);
+            self.cost.stage_grad_x(x, u, du);
+        }
         Ok(())
     }
 
